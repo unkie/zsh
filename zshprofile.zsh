452a4322fb997f52a63456fcf3e59dbcc2934e79
Mark Pustjens <pustjens@dds.nl>
2026-09-15 18:18:58 +0200

    Moved configs for gui from zprofile to hyprland env config.

--- zshprofile.zsh
+++ zshprofile.zsh
@@ -4,23 +4,4 @@
 #
 # Loaded when the shell is a login shell
 
-# This fixes many sdl based games with mouse jumping issues.
-export SDL_MOUSE_RELATIVE=0
-
-# SDL should use pulse for audio out
-export SDL_AUDIODRIVER="pulse"
-
-# Prevent wine front creating file associations and menu items.
-export WINEDLLOVERRIDES='winemenubuilder.exe=d'
-
-# Use GTK2 theme for QT5 and QT6
-export QT_QPA_PLATFORMTHEME=qt6ct
-
-# QT apps should use the wayland backend, and fall back to X11
-export QT_QPA_PLATFORM='wayland;xcb'
-
-# use this app to ask for a password
-if [[ -x /usr/lib/ssh/gnome-ssh-askpass3 ]]; then
-	export SSH_ASKPASS=/usr/lib/ssh/gnome-ssh-askpass3
-fi
-
+# currently empty
